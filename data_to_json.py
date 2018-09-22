f419315dde747ea98b4a0d1cb305cb704c526b91
Vinohith <vinohith1998@gmail.com>
2018-09-22 22:03:52 +0530

    Added data_to_json.py

---
+++ data_to_json.py
@@ -0,0 +1,56 @@
+import json
+
+base_content = [
+	{
+		"Name": "Debian",
+		"Version": "9",
+		"Install": "apt",
+		"Owner": "SPI",
+		"Kernel": "4.9"
+	},
+	{
+		"Name": "Ubuntu",
+		"Version": "17.10",
+		"Install": "apt",
+		"Owner": "Canonical",
+		"Kernel": "4.13"
+	},
+	{
+		"Name": "Fedora",
+		"Version": "26",
+		"Install": "dnf",
+		"Owner": "Red Hat",
+		"Kernel": "4.13"
+	},
+	{
+		"Name": "CentOS",
+		"Version": "7",
+		"Install": "yum",
+		"Owner": "Red Hat",
+		"Kernel": "3.10"
+	},
+	{
+		"Name": "OpenSUSE",
+		"Version": "42.3",
+		"Install": "zypper",
+		"Owner": "Novell",
+		"Kernel": "4.4"
+	},
+	{
+		"Name": "Arch Linux",
+		"Version": "Rolling Release",
+		"Install": "pacman",
+		"Owner": "SPI",
+		"Kernel": "4.13"
+	},
+	{
+		"Name": "Gentoo",
+		"Version": "Rolling Release",
+		"Install": "emerge",
+		"Owner": "Gentoo Foundation",
+		"Kernel": "4.12"
+	}
+]
+
+with open('data.json', 'w') as outfile:
+    json.dump(base_content, outfile)
